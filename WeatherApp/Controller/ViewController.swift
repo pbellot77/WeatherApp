004e66f79b95bd9e5eeb7e40b80e640f77eacf7c
Patrick <patrickbellot77@gmail.com>
2017-11-26 21:52:41 -0600

    add network layer

--- WeatherApp/Controller/ViewController.swift
+++ WeatherApp/Controller/ViewController.swift
@@ -8,13 +8,6 @@
 
 import UIKit
 
-enum APIError: Error {
-  case apiError
-  case responseError
-  case jsonDecoder
-  case unknown
-}
-
 class ViewController: UIViewController {
 
   @IBOutlet weak var cityLabel: UILabel!
@@ -26,7 +19,7 @@ class ViewController: UIViewController {
   @IBOutlet weak var descriptionLabel: UILabel!
   @IBOutlet weak var iconImage: UIImageView!
   
-  //let shared = WeatherService.shared
+  let shared = WeatherService.shared
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -41,7 +34,7 @@ class ViewController: UIViewController {
       print("OK pressed")
       let textField = alert.textFields?[0]
       self.cityLabel.text = textField?.text!
-      self.getWeather(forCity: self.cityLabel.text!.uppercased())
+      self.shared.getWeather(forCity: self.cityLabel.text!.uppercased())
     }
     alert.addTextField { (textfield: UITextField) in
       textfield.placeholder = "City Name"
@@ -52,28 +45,7 @@ class ViewController: UIViewController {
     present(alert, animated: true, completion: nil)
   }
   
-  func getWeather(forCity: String) {
-    guard let url = URL(string: "http://api.openweathermap.org/data/2.5/find?q=\(forCity)&units=imperial&APPID=ba639933d32f48c47d197ac099fa0ec4") else { return }
-    let session = URLSession.shared
-    session.dataTask(with: url) { (data, response, error) in
-      guard error == nil else {
-        print("Error: \(APIError.apiError)")
-        return
-      }
-      guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode else {
-        print("Error: \(APIError.responseError)")
-        return
-      }
-      guard let data = data else { return }
-      do {
-        let currentWeather = try JSONDecoder().decode(CurrentWeather.self, from: data)
-        print(currentWeather)
-      } catch {
-        print("Error: \(APIError.jsonDecoder)")
-      }
-    }.resume()
-  }
-
+  
   @IBAction func searchTapped(_ sender: Any) {
     searchCity()
   }
---
+++ WeatherApp/New Group/WeatherService.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Patrick Bellot on 11/26/17.
+//  Copyright © 2017 Polestar Interactive LLC. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error {
+  case apiError
+  case responseError
+  case jsonDecoder
+  case unknown
+}
+
+class WeatherService {
+  
+  private init() {}
+  static let shared = WeatherService()
+  
+  func getWeather(forCity: String) {
+    guard let url = URL(string: "http://api.openweathermap.org/data/2.5/find?q=\(forCity)&units=imperial&APPID=ba639933d32f48c47d197ac099fa0ec4") else { return }
+    let session = URLSession.shared
+    session.dataTask(with: url) { (data, response, error) in
+      guard error == nil else {
+        print("Error: \(APIError.apiError)")
+        return
+      }
+      guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode else {
+        print("Error: \(APIError.responseError)")
+        return
+      }
+      guard let data = data else { return }
+      do {
+        let currentWeather = try JSONDecoder().decode(CurrentWeather.self, from: data)
+        print(currentWeather)
+      } catch {
+        print("Error: \(APIError.jsonDecoder)")
+      }
+      }.resume()
+  }
+
+}
